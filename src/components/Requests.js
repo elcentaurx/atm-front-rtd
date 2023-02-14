591485273ada8687e0303358b9593b1761e39c5b
elcentaurx <luismarquez.ortiz94@gmail.com>
2023-02-14 08:34:06 -0600

    starting, basic structure of the project

---
+++ src/components/Requests.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+class Requests extends Component{
+    render(){
+        return(
+            <h4>Retiro de saldo para el usuario : </h4>
+        )
+    }
+}
---
+++ src/components/Users.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+class Users extends Component{
+    render(){
+        return(
+            <h4>Usuarios registrados en el banco : </h4>
+        )
+    }
+}
