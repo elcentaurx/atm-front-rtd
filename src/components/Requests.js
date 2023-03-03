d5d50ccd742dfad7cf6a0c9ea7ba01d09444e059
elcentaurx <luismarquez.ortiz94@gmail.com>
2023-03-03 13:59:54 -0600

    adding functions create and check founs p/user

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -1,92 +1,67 @@
 import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
-import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import axios from "axios";
+import alertify from 'alertifyjs';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 
-function MydModalWithGrid(props) {
-  return (
-    <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Using Grid in Modal
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="show-grid">
-        <Container>
-          <Row>
-            <Col xs={12} md={8}>
-              .col-xs-12 .col-md-8
-            </Col>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-          </Row>
+let balance = 0
 
-          <Row>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-          </Row>
-        </Container>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
+const CheckBalance = async () => {
+    let id = document.getElementById("nick_name").value
+    await axios
+          .get("https://atmapi.fly.dev/api/founds/"+id)
+          .then((response) => {
+              if(response.data.balance){
+                alertify.success('Transacción exitosa')
+                balance = response.data.balance
+                document.getElementById('result').innerHTML = 'Su saldo es de $'+balance+' MN'
+              }else if(response.data.error){
+                  alertify.error('El usuario no existe')
+              }
+              else{
+                  alertify.error('Error')
+              }
+          }).catch( err => {
+              console.error('ATM error ', err)
+          });
 }
 
-function ModalUser(props) {
-    return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <h4>Centered Modal</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={props.onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-}
-
-function App() {
-  const [modalShow, setModalShow] = useState(false);
-
+function Requests() {
   return (
     <>
-      <Button variant="primary" onClick={() => setModalShow(true)}>
-        Retirar
-      </Button>
+        <Row className="justify-content-md-center mb-3">
+          <Col></Col>
+          <Col xs lg="2">
+            <Form.Label className="md-6" htmlFor="input_nick_name">Id de usuario</Form.Label>
+            <Form.Control
+              type="text"
+              id="nick_name"
+            />
+          </Col>
+          <Col></Col>
+        </Row>
 
-      <ModalUser
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-      />
+        <Row className="justify-content-md-center mb-3">
+          <Col></Col>
+          <Col>
+          <Button variant="success" onClick={(e) => CheckBalance()}>Buscar</Button>{' '}
+          </Col>
+          <Col></Col>
+        </Row>
+
+        <Row className="justify-content-md-center mb-3">
+          <Col></Col>
+          <Col id="result">
+            
+          </Col>
+          <Col></Col>
+        </Row>
+
     </>
   );
 }
 
-render(<App />);
+export default Requests
